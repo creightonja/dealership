dd0fa23da455ecedf86c395089f105601b4105d3
Jason Creighton & Kevin Tokheim <student@epicodus.com>
2015-08-06 12:08:03 -0700

    working car silex with twig

--- app/app.php
+++ app/app.php
@@ -2,38 +2,26 @@
   require_once __DIR__."/../vendor/autoload.php";
   require_once __DIR__."/../src/car.php";
 
+  session_start();  //Creating cookie for session
+    if (empty($_SESSION['list_of_cars'])) {
+      $_SESSION['list_of_cars'] = array();
+    }
+
   $app = new Silex\Application();
 
-  $app->get("/", function() {
-    return "
-    <!DOCTYPE html>
-    <html>
-      <head>
-  <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
-  <title>Search for a car!</title>
-</head>
+  //Twig Path
+  $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+  ));
 
-<body>
-  <div class='container'>
-    <h1>Car Search</h1>
-    <p>Enter the price and mileage in a car that you are looking for.</p>
-    <form action='/cars'>
-      <div class='form-group'>
-        <label for='user_price'>Enter a price:</label>
-        <input id='user_price' name='user_price' class='form-control' type='number'>
-      </div>
-      <div class='form-group'>
-        <label for='user_miles'>Enter a mileage:</label>
-        <input id='user_miles' name='user_miles' class='form-control' type='number'>
-      </div>
-      <button type='submit' class='btn-success'>Search</button>
-    </form>
-</body>
-<html>";
+  //Route and Controller
+
+  $app->get("/", function() use ($app) {
+    return $app['twig']->render('carsearch.html.twig');
 });
 
-
-  $app->get("/cars", function() {
+  // Setting for loop to get user inputs, test against car data, and save into new_car_array
+  $app->get("/cars", function() use ($app) {
     $user_price = $_GET["user_price"];
     $user_miles = $_GET["user_miles"];
     $porsche = new Car("2014 Porsche 911", 114991, 7864, "img/porsche.jpg");
@@ -42,32 +30,23 @@
     $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979, "/img/cls550.jpg");
     $cars = array($porsche, $ford, $lexus, $mercedes);
 
-    $output = "";
+    $new_car_array = array();
+
     $counter = 0;
     foreach ($cars as $specific_car) {
       if ($specific_car->certainSpecs($user_price, $user_miles)) {
         $counter++;
-        // $car_price = $specific_car->getPrice();
-        // $car_make = $specific_car->getMake();
-        // $car_miles = $specific_car->getMiles();
-        // $car_picture = $specific_car->getPicture();
-        $output = $output .
-            "<div><img src='" . $specific_car->getPicture() . "'</div>
-            <p>" . $specific_car->getMake() . "</p>
-            <p>". $specific_car->getMiles() ." miles</p>
-            <p>$" . $specific_car->getPrice() . "</p>
-            ";
-      }
-    }
-    if ($counter == 0) {
-        $output = $output .
-        "<p>Your search matched zero results</p>";
-    }
+        $new_car_array = new Car($specific_car->getPicture(), $specific_car->getMake(),
+                    $specific_car->getMiles(), $specific_car->getPrice());
+        $new_car_array->save();
+            };
+        };
 
-    return $output;
+        $searchcar = Car::getAll();
+
+    return  $app['twig']->render('cardisplay.html.twig', array('searchcar' => $new_car_array));
+
   });
-
-
 
   return $app;
 ?>
--- src/car.php
+++ src/car.php
@@ -8,43 +8,53 @@ class Car
   private $miles;
   private $picture;
 
+  //Constructor
   function __construct($make_model, $price, $miles, $picture) {
     $this->make_model = $make_model;
     $this->price = $price;
     $this->miles = $miles;
     $this->picture = $picture;
   }
+
+  //Setters
   function setPrice($new_price) {
     $float_price = (float) $new_price;
     if ($float_price !=0) {
       $formatted_price = number_format($float_price, 2);
       $this->price = $formatted_price;
-    }
+  }
+
+  function setMake($new_make){
+    $this->make = $new_make;
+  }
+
+  function setMiles($new_miles){
+    $this->miles = $new_miles;
+  }
+
+  function setPicture($new_picture){
+    $this->picture = $new_picture;
+  }
+
+  //Getters
   }
   function getPrice() {
     return $this->price;
   }
-  function setMake($new_make){
-    $this->make = $new_make;
-  }
+
   function getMake() {
     return $this->make_model;
   }
-  function setMiles($new_miles){
-    $this->miles = $new_miles;
-  }
+
   function getMiles() {
     return $this->miles;
   }
-  function setPicture($new_picture){
-    $this->picture = $new_picture;
-  }
+
   function getPicture() {
     return $this->picture;
   }
 
-
-  
+  //If/Else Statement
   function certainSpecs($user_price, $user_miles) {
 
     if (($this->price <= $user_price) && ($this->miles <= $user_miles)) {
@@ -54,41 +64,23 @@ class Car
       return false;
     }
   }
+
+  //Save Method
+  function save()
+  {
+    array_push($_SESSION['list_of_cars'], $this);
+  }
+
+  //Getter Static Method
+  static function getAll()
+  {
+    return $_SESSION['list_of_cars'];
+  }
+
+  //Static Method - Deletes Tasks
+  static function deleteAll()
+  {
+    $_SESSION['list_of_cars'] = array();
+  }
+
 }
-/*
-$porsche = new Car("2014 Porsche 911", 114991, 7864, "img/porsche.jpg");
-$ford = new Car("2011 Ford F450", 55995, 14241, "/img/ford.jpg");
-$lexus = new Car("2013 Lexus RX 350", 44700, 20000, "/img/lexus.jpg");
-$mercedes = new Car("Mercedes Benz CLS550", 39900, 37979, "/img/cls550.jpg");
-$cars = array($porsche, $ford, $lexus, $mercedes);
-?>
-<!DOCTYPE html>
-<html>
-<head>
-</head>
-<body>
-  <div class="container">
-    <?php
-      $counter = 0;
-      foreach ($cars as $specific_car) {
-        if ($specific_car->certainSpecs($user_price, $user_miles)) {
-          $counter++;
-          $car_price = $specific_car->getPrice();
-          $car_make = $specific_car->getMake();
-          $car_miles = $specific_car->getMiles();
-          $car_picture = $specific_car->getPicture();
-          echo "<div><img src='$car_picture'</div>
-                <p>$car_make</p>
-                <p>$car_miles miles</p>
-                <p>$$car_price</p>
-              ";
-        }
-      }
-        if ($counter == 0) {
-          echo "<p>Your search matched zero results</p>";
-        }
-    ?>
-  </div>
-</body>
-</html>
-*/
